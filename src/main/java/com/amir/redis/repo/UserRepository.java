b2b7b28cb16b4ece54be9566027ff8103454571f
mohammadaamir1102 <er.aamir1102@gmail.com>
2024-10-31 02:23:31 +0530

    annotation base config issue fixed and commented unwanted configs and codes

--- src/main/java/com/amir/redis/repo/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.amir.redis.repo;
-
-import com.amir.redis.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends JpaRepository<User, Long> {
-}
--- src/main/java/com/amir/redis/res/UserResponse.java
+++
@@ -1,9 +0,0 @@
-package com.amir.redis.res;
-
-import lombok.Builder;
-
-import java.time.LocalDate;
-
-@Builder
-public record UserResponse(Long id, String fullName, String email, LocalDate dob, int age) {
-}
